fa68c767a70c0d28c0ecc26b0d8eb9a81dda0f62
GLuca27 <gianluca.sciglitano1@gmail.com>
2022-10-26 12:47:06 +0200

    non ne ho saputo fare nemmeno uno

---
+++ jsnack3/script.js
@@ -0,0 +1,13 @@
+//In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+const user = prompt("Inserisci il tuo nome")
+
+const invitati = ["Samuele", "Gianluca", "Marco", "Giovanni"];
+for (let i=0; i<4; i++){
+    console.log(invitati[i]);
+} if (found){
+    alert("sei stato invitato")
+} else {
+    alert("non sei stato invitato")
+}
+
+

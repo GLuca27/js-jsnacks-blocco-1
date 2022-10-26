3bf0f9893bbbd4b59238adfe8de62663a5a840a9
GLuca27 <gianluca.sciglitano1@gmail.com>
2022-10-26 11:27:56 +0200

    mi sono perso in un bicchiere d'acqua, non ricopio per correttezza

---
+++ jsnack1/script.js
@@ -0,0 +1,7 @@
+//L’utente inserisce due parole in successione, con due prompt.
+//Il software stampa prima la parola più corta, poi la parola più lunga.
+
+const firstWord = prompt("Inserisci la prima parola")
+
+const secondWord = prompt("Inserisci la seconda parola")
+console.log(firstWord, secondWord)
